ab9106fc3ba63dd5508e19b8f59d675a1b82a987
Vishnu Kugarthi <vishnutejaap@gmail.com>
2022-02-16 15:42:23 +0530

    copied complete folder from buy-tools-smart

--- components/header-react/src/App.js
+++ components/header-react/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const styles = `
+.buy-tools {
+  background: lightgreen;
+  padding: 1em;
+}
+.container {
+  background: rgb(189, 247, 171);
+  padding: 1em;
+}
+
+.btn {
+  width: 100%;
+  margin: 0.5em;
+  padding: 0.5em;
+  border-radius: 5px;
+}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>Vishnu K </header>
     </div>
   );
 }
--- components/header-react/src/index.js
+++ components/header-react/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default class HeaderReactComponent extends HTMLElement {
+  connectedCallback() {
+    this.root = this.attachShadow({ mode: 'open' });
+    this.render();
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  disconnectedCallback() {
+    ReactDOM.unmountComponentAtNode(this);
+  }
+
+  render() {
+    ReactDOM.render(<App />, this.root);
+  }
+}
+
+customElements.define('header-react', HeaderReactComponent);
