78b86b48b748af581ee46679b24186b7a02411a0
Vishnu Kugarthi <vishnutejaap@gmail.com>
2022-02-16 16:11:08 +0530

    Able to send data from host index.html to react app

--- components/header-react/src/App.js
+++ components/header-react/src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
 
-const styles = `
-.buy-tools {
-  background: lightgreen;
-  padding: 1em;
-}
-.container {
-  background: rgb(189, 247, 171);
-  padding: 1em;
-}
-
-.btn {
-  width: 100%;
-  margin: 0.5em;
-  padding: 0.5em;
-  border-radius: 5px;
-}
-`;
-
-function App() {
+function App({ count }) {
   return (
     <div>
-      <header>Vishnu K </header>
+      <header>Vishnu T K </header>
+      {/* <p>List of persons = {{ persons }}</p> */}
+      <p>
+        Name of person ={' '}
+        {document.querySelector('header-react').attributes['persons'].value}
+      </p>
     </div>
   );
 }
