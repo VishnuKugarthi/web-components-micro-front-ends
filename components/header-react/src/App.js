a4b4fec93c0a23afa3cd142246d4bf93f1712efd
Vishnu Kugarthi <vishnutejaap@gmail.com>
2022-02-16 20:56:36 +0530

    styles applying to shadow dom

--- components/header-react/src/App.js
+++ components/header-react/src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 // import './App.css';
-import styles from './App.module.css';
+// import styles from './App.module.css';
 
-{
-  /* <style>
-.headerStyle{
-    background-color:orange;
+const styles = `
+.buy-tools {
+  background: lightgreen;
+  padding: 1em;
 }
-</style> */
+.container {
+  background: violet;
+  padding: 1em;
 }
+
+.btn {
+  width: 100%;
+  margin: 0.5em;
+  padding: 0.5em;
+  border-radius: 5px;
+}
+`;
 
 function App({ person }) {
   return (
-    <div className='headerStyle'>
-      <header>Vishnu T K </header>
-      <p>Name of person ={person} </p>
+    <div className='container'>
+      <style dangerouslySetInnerHTML={{ __html: styles }}></style>
+      <div>
+        <header>Vishnu T K </header>
+        <p>Name of person ={person} </p>
+      </div>
     </div>
   );
 }
